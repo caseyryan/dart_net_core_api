c2faea7f08c2ac397cb92aa5ae8b154315c174ba
Konstantin <konstantin.p.serov@yandex.ru>
2024-11-25 13:16:04 +0700

    add postgres config

--- lib/configs/postgresql_config.dart
+++ lib/configs/postgresql_config.dart
@@ -1,5 +1,14 @@
 import 'package:dart_net_core_api/config.dart';
 
 class PostgreSQLConfig implements IConfig {
+  String? user;
+  String? password;
+  String? host;
+  int? port;
+  String? database;
+
   
+  String get connectionString {
+    return 'postgres://$user:$password@$host:$port/$database';
+  }
 }
